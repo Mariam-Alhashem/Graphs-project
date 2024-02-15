6097b8f98a743a04f34dc5fc9c8a59b88b3dd345
Zephzhao328 <159911400+Zephzhao328@users.noreply.github.com>
2024-02-15 16:42:45 +0000

    Update Social-network.py

--- Social-network.py
+++ Social-network.py
@@ -5,31 +5,30 @@ class Graph:
     def __init__(self):
         self.graph = {}
 
-#node representing a member 
-#node class
-def add_member(self, member):
-        if member not in self.graph:
-            self.graph[member] = []
 
-def add_relationship(self, member1, member2):
-        if member1 in self.graph and member2 in self.graph:
-            if member2 not in self.graph[member1]:
-                self.graph[member1].append(member2)
-            if member1 not in self.graph[member2]:
-                self.graph[member2].append(member1)
-        else:
-            print("One or both members not found in the network.")
+
+
     
 
 
 #edge representing a relationship
 
 #method to add new member (name + any info)
-
+def add_member(self, member):
+    if member not in self.graph:
+        self.graph[member] = []
 #method to add relationship b/w members
-
+def add_relationship(self, member1, member2):
+    if member1 in self.graph and member2 in self.graph:
+        if member2 not in self.graph[member1]:
+            self.graph[member1].append(member2)
+        if member1 not in self.graph[member2]:
+            self.graph[member2].append(member1)
+        else:
+            print("One or both members not found in the network.")
 #method to find friends of a member (list of all the friends)
-
+def find_friends(self,member):
+    print(self.graph[member])
 #method to find a path between 2 members
 
 
