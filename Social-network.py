84a1cb3a69d30e16f3676f62eab54f07b35811e8
Shagul-Sajjad <157045351+Shagul-Sajjad@users.noreply.github.com>
2024-02-15 08:34:17 -0800

    Update Social-network.py
    starting the code

--- Social-network.py
+++ Social-network.py
@@ -1,7 +1,26 @@
 #graph class
   print("hello world")
+#graph class
+class Graph:
+    def __init__(self):
+        self.graph = {}
 
 #node representing a member 
+#node class
+def add_member(self, member):
+        if member not in self.graph:
+            self.graph[member] = []
+
+def add_relationship(self, member1, member2):
+        if member1 in self.graph and member2 in self.graph:
+            if member2 not in self.graph[member1]:
+                self.graph[member1].append(member2)
+            if member1 not in self.graph[member2]:
+                self.graph[member2].append(member1)
+        else:
+            print("One or both members not found in the network.")
+    
+
 
 #edge representing a relationship
 
