a607ef4f44f604a0b88f84fbc040a14d27481107
Zephzhao328 <159911400+Zephzhao328@users.noreply.github.com>
2024-02-16 21:23:26 +0000

    Update Network-connectivity.py

--- Network-connectivity.py
+++ Network-connectivity.py
@@ -6,12 +6,22 @@ class NetworkGraph:
        if computer not in self.graph:
            self.graph[computer] = []
        # a helper method for is fully connected method 
-       def dfs(self, node, visited):
-              if node not in visited:
-                  visited.add(node)
-                  for neighbor in self.graph[node]:
-                      self.dfs(neighbor, visited)
-
+   def dfs(self, node, visited):
+       if node not in visited:
+            visited.add(node)
+            for neighbor in self.graph[node]:
+                self.dfs(neighbor, visited)
+   def add_connection(self, computer1, computer2):
+       if computer1 in self.graph and computer2 in self.graph:
+             self.graph[computer1].append(computer2)
+             self.graph[computer2].append(computer1)
+   def is_fully_connected(self):
+       if not self.graph:
+           return False
+       start_node = next(iter(self.graph))
+       visited = set()
+       self.dfs(start_node, visited)
+       return len(visited) == len(self.graph)
 
 # test: Create a network graph
 network = NetworkGraph()
